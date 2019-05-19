26f265b7ed921f8d6d706ef138ba83a961916ab3
kamnan <kamile.nanartonyte@ktu.edu>
2019-05-19 14:33:58 +0300

    Pataisytas enum

--- Liftai0513/LiftaiMVC/Models/Elevator.cs
+++ Liftai0513/LiftaiMVC/Models/Elevator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
+using System.Reflection;
 
 namespace LiftaiMVC.Models
 {
@@ -85,7 +86,27 @@ namespace LiftaiMVC.Models
             }
             return false;
         }
-        
+
+        public string StateName()
+        {
+            return this.State.GetAttribute<DisplayAttribute>().Name;
+        }
+    }
+
+    public static class Extensions
+    {
+        /// <summary>
+        ///     A generic extension method that aids in reflecting 
+        ///     and retrieving any attribute that is applied to an `Enum`.
+        /// </summary>
+        public static TAttribute GetAttribute<TAttribute>(this Enum enumValue)
+                where TAttribute : Attribute
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<TAttribute>();
+        }
     }
 
     public enum States
